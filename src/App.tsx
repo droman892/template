ca069b10a6948ae3b69a53b7ad12ac93a7c9deb2
droman892 <droman892@gmail.com>
2021-12-15 17:07:29 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import MediaImg from './assets/images/media.png'
 import SunSVG from './assets//images/sun.svg'
 
 export const App = () => {
-    const person = 'Tech Wizzzzzz'
+    const person = 'Tech Wiz'
     return (
         <div>
             <h1>
-                WOW, the app is working, {person}!!!! - {process.env.NODE_ENV}{' '}
+                The app is ready for you, {person}! - {process.env.NODE_ENV}{' '}
                 {process.env.name}
             </h1>
-            <p>Welcome to the template!!!!!</p>
+            <p>
+                This template comes with Webpack, TypeScript, Babel, ESLint, and
+                Prettier.
+            </p>
             <Counter />
             <img src={MediaImg} alt="Media Icons PNG"></img>
             <img src={SunSVG} alt="Sun SVG"></img>
